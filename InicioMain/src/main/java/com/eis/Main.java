f0760eb4750e7368d559518b799a67e0986aea50
alanfg <aherrera@asesoftware.com>
2024-11-22 18:12:24 -0500

    factory and pom

--- InicioMain/src/main/java/com/eis/Main.java
+++ InicioMain/src/main/java/com/eis/Main.java
@@ -1,23 +1,31 @@
 package com.eis;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Documento documento=generarDocumentos();
+        var documentos = generarDocumentos();
+       for (var documento : documentos) {
+           documento.dibujar();
+           documento.imprimir();
+       }
 
-        documento.dibujar();
-        documento.imprimir();
     }
 
-    public static Documento generarDocumentos(){
-        Documento documento;
+    public static List<Documento> generarDocumentos(){
+        List<Documento> documentos = new ArrayList<>();
         var factory = new FactoryDocument();
-        documento= factory.getDocument("Text");
-        documento.setContenido("...texto...");
-        documento= factory.getDocument("HTML");
-        documento.setContenido("<HMTL></HMTL>");
-        documento= factory.getDocument("PDF");
-        documento.setContenido("...PDF...PDF...PDF...");
-        return documento;
+        var documentoText= factory.getDocument("Text");
+        documentoText.setContenido("...texto...");
+        documentos.add(documentoText);
+        var documentoHtml= factory.getDocument("HTML");
+        documentoHtml.setContenido("<HMTL>Hola soy html</HMTL>");
+        documentos.add(documentoHtml);
+        var documentoPDF= factory.getDocument("PDF");
+        documentoPDF.setContenido("...PDF...PDF...PDF...");
+        documentos.add(documentoPDF);
+        return documentos;
     }
 }
